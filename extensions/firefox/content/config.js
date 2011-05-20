ed03baa5601e5914599ef48f5aee5760d2d9da17
Teodor Pripoae <toni@macbook.(none)>
2011-05-20 23:25:33 +0300

    Devcommit

--- extensions/firefox/content/config.js
+++
@@ -1,3 +0,0 @@
-var SERVER_URL = 'http://localhost:8000';
-var UPDATE_URL = SERVER_URL + '/api/send-page/';
-var SEARCH_URL = SERVER_URL + '/api/most-visited/';
--- extensions/firefox/content/popup.js
+++
@@ -1,9 +0,0 @@
-function populate_popup (response) {
-  //Firebug.Console.log(document.getElementById("most-visited"));
-  alert(response);
-  var obj = jQuery.parseJSON(response);
-  for (i = 0; i < obj.pages.length; i ++) {
-    alert(obj.pages[i].long_url);
-  }
-//  alert(obj.pages);
-}
